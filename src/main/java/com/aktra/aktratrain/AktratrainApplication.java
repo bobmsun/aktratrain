df31f6b4ac7972c281d4f199b58c5561333be567
Shihao Sun <shihao.m.sun@gmail.com>
2026-03-08 00:21:17 -0500

    Initial Commit

---
+++ src/main/java/com/aktra/aktratrain/AktratrainApplication.java
@@ -0,0 +1,13 @@
+package com.aktra.aktratrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AktratrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AktratrainApplication.class, args);
+	}
+
+}
