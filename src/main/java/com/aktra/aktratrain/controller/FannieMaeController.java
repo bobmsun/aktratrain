6739a718cc8772681d44f414cc12d8237b5f4ae3
Shihao Sun <shihao.m.sun@gmail.com>
2026-03-08 21:50:48 -0400

    Add log in the fannie mae controller

--- src/main/java/com/aktra/aktratrain/controller/FannieMaeController.java
+++ src/main/java/com/aktra/aktratrain/controller/FannieMaeController.java
@@ -2,12 +2,18 @@ package com.aktra.aktratrain.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 public class FannieMaeController {
 
+    private static final Logger logger = LoggerFactory.getLogger(FannieMaeController.class);
+
     @GetMapping("/fannie-mae")
     public String showAMTMPage() {
+        logger.info("In the FannieMaeController, handling GET request for /fannie-mae");
+        logger.info("About to return the Fannie Mae page");
         return "fannie-mae";
     }
 }
